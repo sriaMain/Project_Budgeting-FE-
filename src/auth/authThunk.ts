423a21be10abd67a4fe29c98fa2191f09de3c67a
shivasurya-1 <shivasurya821@gmail.com>
2025-12-05 21:27:41 +0530

    modify changes

--- src/auth/authThunk.ts
+++ src/auth/authThunk.ts
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../utils/axiosInstance";
-import axiosRefresh from "../utils/axiosInstance";
 
 
 interface   InitializeAuthResponse {
@@ -18,14 +17,14 @@ export const initializeAuth = createAsyncThunk<
   async (_, thunkAPI) => {
     try {
       console.log("Initializing Auth");
-      const response = await axiosRefresh.post("/accounts/refresh/", {}, { withCredentials: true });
-
+      const response = await axiosInstance.post("/accounts/refresh/");
       console.log("Auth Initialized:", response.data);
      
-      return {
-        isAuthenticated: true,
-        userRole: response.data.role,
-        accessToken: response.data.access_token,
+        return {
+          isAuthenticated: true,
+          userRole: response.data.role,
+          accessToken: response.data.token,
+        
       }
     } catch (error) {
     
